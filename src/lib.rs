49bceeaed55da7c2e97f27a882695f6bb6f5cf50
Bazaah <github@luxolus.com>
2019-10-30 11:22:12 +0000

    machine #3

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,42 @@
-trait Shared {
-    fn functionality(&self) -> usize;
+struct Machine<S = Waiting> {
+    shared: usize,
+    state: S,
 }
 
-#[derive(Default, Debug)]
+impl Default for Machine {
+    fn default() -> Self {
+        Self {
+            shared: 0,
+            state: Waiting::default(),
+        }
+    }
+}
+
+impl From<Machine<Waiting>> for Machine<Filling> {
+    fn from(m: Machine<Waiting>) -> Self {
+        Self {
+            shared: m.shared,
+            state: Filling { rate: 1 },
+        }
+    }
+}
+
+impl From<Machine<Filling>> for Machine<Done> {
+    fn from(m: Machine<Filling>) -> Self {
+        Self {
+            shared: m.shared,
+            state: Done,
+        }
+    }
+}
+
+#[derive(Default)]
 struct Waiting {
     duration: usize,
-    shared: usize
 }
 
-impl Waiting {
-    fn to_filling(self) -> Filling {
-        Filling {
-            rate: 1,
-            shared: self.shared
-        }
-    } 
-}
-
-impl Shared for Waiting {
-    fn functionality(&self) -> usize {
-        self.shared
-    }
-}
-
-#[derive(Default, Debug)]
 struct Filling {
     rate: usize,
-    shared: usize,
 }
 
-impl Filling {
-    fn to_done(self) -> Done {
-        Done {
-            shared: self.shared
-        }
-    }
-}
-
-impl Shared for Filling {
-    fn functionality(&self) -> usize {
-        self.shared
-    }
-}
-
-#[derive(Default, Debug)]
-struct Done {
-    shared: usize
-}
-
-impl Shared for Done {
-    fn functionality(&self) -> usize {
-        self.shared
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn transistion() {
-        let state = Waiting { duration: 0, shared: 42 };
-        assert!(state.functionality() == 42);
-
-        let state = state.to_filling();
-
-        assert!(state.functionality() == 42);
-
-        let state = state.to_done();
-
-        assert!(state.functionality() == 42);
-    }
-}
+struct Done;

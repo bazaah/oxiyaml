e376ed5e9f31bd140413fc06e021581581577dca
Bazaah <github@luxolus.com>
2019-10-30 14:51:41 +0000

    machine #4 - lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,134 @@
-struct Machine<S = Waiting> {
-    shared: usize,
+use std::marker::PhantomData;
+
+struct StateMachine<'state, S = StateA<'state>>
+where
+    S: Lifetime<'state> + 'state,
+{
+    data: String,
     state: S,
+    mkr: PhantomData<&'state S>,
 }
 
-impl Default for Machine {
+impl<'state> StateMachine<'state> {
+    fn with_words(words: &'state str) -> Self {
+        StateMachine {
+            data: format!("dank memes"),
+            state: StateA { value: words },
+            mkr: PhantomData,
+        }
+    }
+}
+
+impl<'state> Default for StateMachine<'state> {
     fn default() -> Self {
         Self {
-            shared: 0,
-            state: Waiting::default(),
+            data: format!("dank memes"),
+            state: Default::default(),
+            mkr: PhantomData,
         }
     }
 }
 
-impl From<Machine<Waiting>> for Machine<Filling> {
-    fn from(m: Machine<Waiting>) -> Self {
-        Self {
-            shared: m.shared,
-            state: Filling { rate: 1 },
+impl<'state, S> Lifetime<'state> for StateMachine<'state, S>
+where
+    S: Lifetime<'state> + 'state,
+{
+    fn value(&'state self) -> &'state str {
+        self.state.value()
+    }
+}
+
+impl<'state> From<StateMachine<'state, StateA<'state>>> for StateMachine<'state, StateB<'state>> {
+    fn from(s: StateMachine<'state, StateA<'state>>) -> Self {
+        StateMachine {
+            data: s.data,
+            state: StateB::new(s.state.value),
+            mkr: PhantomData,
         }
     }
 }
 
-impl From<Machine<Filling>> for Machine<Done> {
-    fn from(m: Machine<Filling>) -> Self {
-        Self {
-            shared: m.shared,
-            state: Done,
+impl<'state> From<StateMachine<'state, StateB<'state>>> for StateMachine<'state, StateC> {
+    fn from(s: StateMachine<'state, StateB<'state>>) -> Self {
+        StateMachine {
+            data: s.data,
+            state: StateC::new(&s.state.split),
+            mkr: PhantomData,
         }
     }
+}
+
+trait Lifetime<'state> {
+    fn value(&'state self) -> &'state str;
 }
 
 #[derive(Default)]
-struct Waiting {
-    duration: usize,
+struct StateA<'state> {
+    value: &'state str,
 }
 
-struct Filling {
-    rate: usize,
+impl<'state> Lifetime<'state> for StateA<'state> {
+    fn value(&'state self) -> &'state str {
+        self.value
+    }
 }
 
-struct Done;
+struct StateB<'state> {
+    split: Vec<&'state str>,
+}
+
+impl<'state> StateB<'state> {
+    fn new(src: &'state str) -> Self {
+        Self {
+            split: src.split(' ').collect(),
+        }
+    }
+}
+
+impl<'state> Lifetime<'state> for StateB<'state> {
+    fn value(&'state self) -> &'state str {
+        self.split[0]
+    }
+}
+
+struct StateC {
+    last: String,
+}
+
+impl StateC {
+    fn new<S: AsRef<str>>(words: &[S]) -> Self {
+        let last =
+            words
+                .iter()
+                .map(|s| s.as_ref())
+                .rev()
+                .fold(String::default(), |mut last, word| {
+                    last += word;
+                    last
+                });
+        StateC { last }
+    }
+}
+
+impl<'state> Lifetime<'state> for StateC {
+    fn value(&'state self) -> &'state str {
+        self.last.as_str()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check() {
+        let state = StateMachine::with_words("dank memes boi");
+        assert_eq!(state.value(), "dank memes boi");
+
+        let state: StateMachine<StateB> = state.into();
+        assert_eq!(state.value(), "dank");
+
+        let state: StateMachine<StateC> = state.into();
+        assert_eq!(state.value(), "boimemesdank")
+    }
+}
